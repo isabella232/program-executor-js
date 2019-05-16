75b5d9babe6575bbf46aec36f8af4d46cc1b7c88
Marton Papp <marton.papp@emarsys.com>
2019-05-16 09:52:57 +0200

    fix(package): fix type definitions CDPIP-1372

--- types.d.ts
+++ types.d.ts
@@ -1,25 +1,25 @@
 /**
  * @param {object} config
- * @param {object} object.knex - Connected Knex instance
- * @param {string} object.amqpUrl - RabbitMq Url
- * @param {string} object.tableName - Table name for bookkeeping
- * @param {string} object.queueName - Queue name to publish to
+ * @param {object} config.knex - Connected Knex instance
+ * @param {string} config.amqpUrl - RabbitMq Url
+ * @param {string} config.tableName - Table name for bookkeeping
+ * @param {string} config.queueName - Queue name to publish to
  */
 declare class ProgramExecutor {
     constructor(config: any);
     /**
      * @param {object} data
-     * @param {object} object.programData
-     * @param {array} object.jobs
-     * @param {object} object.jobsData
+     * @param {object} data.programData
+     * @param {array} data.jobs
+     * @param {object} data.jobsData
      */
     createProgram(data: any): void;
     /**
      * @param {object} config
-     * @param {object} object.knex - Connected Knex instance
-     * @param {string} object.amqpUrl - RabbitMq Url
-     * @param {string} object.tableName - Table name for bookkeeping
-     * @param {string} object.queueName - Queue name to publish to
+     * @param {object} config.knex - Connected Knex instance
+     * @param {string} config.amqpUrl - RabbitMq Url
+     * @param {string} config.tableName - Table name for bookkeeping
+     * @param {string} config.queueName - Queue name to publish to
      */
     static create(config: any): void;
 }
